b6fddcef52ac0ffbbe5978406b77dc7997db9b23
anataayn <anata_ayuni_30rpl@student.smktelkom-mlg.sch.id>
2023-04-06 11:18:20 +0700

    first commit

--- app/src/main/java/com/example/mytwice/MembersData.kt
+++ app/src/main/java/com/example/mytwice/MembersData.kt
@@ -3,50 +3,52 @@ package com.example.mytwice
 object MembersData {
     private val data = arrayOf(
         arrayOf(
-            "Kai",
-            "Kim Jong-in, atau yang lebih dikenal dengan nama Kai, adalah penyanyi, aktor, dan penari Korea Selatan. Ia adalah anggota grup penyanyi Korea Selatan-Tiongkok EXO dan subkelompok-nya EXO-K.",
-            "https://upload.wikimedia.org/wikipedia/commons/0/0c/Kai_-_2016_Gaon_Chart_K-pop_Awards_red_carpet.jpg",
+            "Junhyung",
+            "adalah penyanyi-penulis lagu, rapper, produser rekaman, dan aktor Korea Selatan. Junhyung menjadi terkenal sebagai anggota boy band Korea Selatan Highlight (sebelumnya dikenal sebagai Beast), yang dibentuk pada tahun 2009.",
+            "https://upload.wikimedia.org/wikipedia/commons/b/bf/Yong_Jun-hyung_2018-05-30.jpg",
         ),
         arrayOf(
-            "Baekhyun",
-            "Byun Baek-hyun, atau lebih dikenal dengan mononim Baekhyun adalah penyanyi, penulis lagu, dan aktor Korea Selatan. Pada tahun 2012, Baekhyun memulai debut sebagai anggota grup Korea Selatan-Tiongkok, EXO. Ia juga tergabung dalam dua subunit EXO, yakni EXO-K dan EXO-CBX.Ia juga merupakan leader dari grup SuperM.",
-            "https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcQ1WO39bPfKKglF3Q6PwwvZUr9ny3TbyL3YiyI6u-eyYUj1tqt72WsKEg8&usqp=CAE&s=19",
+            "Dongjun",
+            "adalah penyanyi dan aktor Korea Selatan. Dia memulai debutnya sebagai anggota boy grup ZE:A (dan kemudian subgrupnya ZE:A Five dan ZE:AJ).",
+            "https://upload.wikimedia.org/wikipedia/commons/2/24/2017_%EC%95%84%EC%8B%9C%EC%95%84_%EB%AA%A8%EB%8D%B8_%ED%8E%98%EC%8A%A4%ED%8B%B0%EB%B2%8C_%EB%A0%88%EB%93%9C%EC%B9%B4%ED%8E%AB_%2872%29.jpg",
         ),
         arrayOf(
-            "D.O.",
-            "Doh Kyung-soo, juga secara profesional dikenal sebagai D.O., adalah penyanyi dan aktor Korea Selatan, paling dikenal sebagai anggota grup vokal pria Korea Selatan-Tiongkok Exo.",
-            "https://cdn1-production-images-kly.akamaized.net/-W1rGWhYJHMl3uT1JgPe61K5KpI=/1200x900/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/2819465/original/004625800_1559189314-do.jpg",
+            "Shin",
+            "adalah aktor Korea Selatan. Salah satu aktor dengan bayaran tertinggi di Korea Selatan, [1] penghargaannya meliputi empat Baeksang Arts Awards , dua Grand Bell Awards , dan satu Blue Dragon Film Award . Dari 2012 hingga 2016 dan pada 2021, ia muncul dalam daftar 40 Selebriti Forbes Korea Power . [2] Ia terpilih sebagai Aktor Televisi Terbaik Gallup Korea Tahun 2014 dan ditampilkan oleh Forbes dalam daftar 30 Under 30 Asia tahun 2016",
+            "https://upload.wikimedia.org/wikipedia/commons/2/2a/Kim_Soo-hyun_in_June_2020.jpg",
         ),
         arrayOf(
-            "Sehun",
-            "Oh Se-hun, lebih dikenal dengan mononim Sehun, adalah penyanyi, rapper, model, dan aktor asal Korea Selatan. Ia dikenal sebagai anggota dari boyband Korea Selatan-Tiongkok EXO dan sub kelompoknya EXO-K.",
-            "https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcT_9kbHv-BqMhp9LuEMfN45uKKQaXgcZ15ztAaf0AT_AbDG0JQwrlLxpoej&usqp=CAE&s=19",
+            "kangsung",
+            "Lee Kang Seong atau dikenal dengan nama Kangsung ini adalah main rapper andalan GHOST9. Kangsung dikenal sebagai member dengan suara yang powerful saat melantunkan rap",
+            "https://static.wikia.nocookie.net/kpop/images/1/13/GHOST9_Lee_Kang_Sung_Arcade_-_V_concept_photo_1.png/revision/latest?cb=20220323200824",
         ),
         arrayOf(
-            "Chanyeol",
-            "Park Chan-yeol, lebih dikenal dengan mononim Chanyeol adalah rapper, penyanyi, penulis lagu, dan aktor Korea Selatan. Ia adalah anggota dari grup penyanyi laki-laki Tiongkok-Korea Selatan EXO dan sub kelompoknya EXO-K.",
-            "https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcSXidtYqKNewlGH7M5XEfLaqYpBqhZyccNv5lU3akeic5nyIut-2hEq_udU&usqp=CAE&s=19",
+            "Junseong",
+            "(juga dikreditkan sebagai Jun Sung Kim, Jun Kim atau Brian Kim; lahir 4 Oktober 1975) adalah aktor Amerika Serikat kelahiran Hong Kong keturunan Korea. Dia mungkin paling dikenal karena perannya sebagai Mike Juhn dalam film kejahatan tahun 2007 West 32nd (2007), yang disutradarai oleh Michael Kang.",
+            "https://static.wikia.nocookie.net/kpop/images/6/60/GHOST9_Choi_Jun_Seong_Arcade_-_V_concept_photo_1.png/revision/latest?cb=20220323200933",
         ),
         arrayOf(
-            "Chen",
-            "Kim Jong-dae, atau lebih dikenal dengan nama panggungnya, Chen, adalah penyanyi dan penulis lagu Korea Selatan. Ia debut pada tahun 2012 sebagai anggota dan vokalis utama grup vokal pria Korea Selatan-Tiongkok, EXO, berserta subunitnya EXO-M.",
-            "https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcTUtZvnjSvkGqX-ljKYtepXKCYKnxCtMpPYcRyQohajf2V_NVniLP9D-3A&usqp=CAE&s=19",
+            "Jinwoo",
+            "lahir 26 September 1991), juga dikenal secara mononim sebagai Jinu , adalah penyanyi, aktor, dan anggota Pemenang Korea Selatan di bawah YG Entertainment . Band ini memulai debutnya pada 17 Agustus 2014 dengan album penuh berjudul 2014 S/S . Kim juga debut sebagai solois pada 14 Agustus 2019 dengan single album berjudul Jinu's Heyday .",
+            "https://kepoper.com/wp-content/uploads/2021/08/JINWOO.png",
         ),
         arrayOf(
-            "Suho",
-            "Kim Jun-myeon, atau lebih dikenal dengan nama panggungnya Suho adalah penyanyi, penulis lagu, dan aktor Korea Selatan. Saat ini ia adalah pemimpin dan vokalis utama grup pria Korea Selatan-Tiongkok, EXO dan subunitnya EXO-K.",
-            "https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcQDQFY4thGBbYxJJMlIQQTGmejfAoqX0CtafKZ4KFIVq514tFHWHwPgrQKm&usqp=CAE&s=19",
+            "Taeseung",
+            "adalah penyanyi Korea Selatan di bawah Maroo Entertainment . Ia merupakan mantan anggota boy grup GHOST9 dan grup unit TEEN TEEN .",
+            "https://static.wikia.nocookie.net/kpop/images/9/9d/GHOST9_Lee_Tae_Seung_Now_-_When_We_Are_In_Love_concept_photo_1.png/revision/latest?cb=20210520174259",
         ),
         arrayOf(
-            "Xiumin",
-            "Kim Min-seok, atau yang lebih dikenal dengan nama panggungnya Xiumin, adalah penyanyi dan aktor Korea Selatan. Ia dikenal sebagai anggota grup musik Korea Selatan-Tiongkok EXO, berserta subunitnya EXO-M dan EXO-CBX.",
-            "https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcSYWBfwIL6QmxJ9n4HnW3hro6JDlX5AqmSt23gfjAk-JXjjAliu7ANS2T9_&usqp=CAE&s=19",
+            "Prince",
+            "adalah penyanyi Thailand di bawah Maroo Entertainment . Dia adalah anggota dari boy grup GHOST9 .",
+            "https://static.wikia.nocookie.net/kpop/images/e/e9/GHOST9_Prince_Arcade_-_V_concept_photo_1.png/revision/latest?cb=20220322195958",
         ),
         arrayOf(
-            "Lay",
-            "Zhang Yixing, atau lebih dikenal dengan nama panggung Lay Zhang atau hanya Lay, adalah seorang penyanyi, rapper, penulis lagu, penari, dan aktor Tiongkok. Ia debut pada tahun 2012 sebagai anggota grup musik pria Korea Selatan-Tiongkok, EXO berserta subunitnya EXO-M di bawah naungan SM Entertainment.",
-            "https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcTKVRMLndbq1srE1jzXUh-xBjl8pLF4HG1euoaUXH06opAZ2LmaSwWQiW5-&usqp=CAE&s=19",
+            "Woojin",
+            "lahir 8 April 1997), juga dikenal sebagai Woojin ( 우진 ), adalah penyanyi dan aktor Korea Selatan. Dia masuk ke JYP Entertainment dan menjadi anggota boy grup Stray Kids dari formasi mereka melalui acara bertahan hidup eponymous pada tahun 2017 hingga kepergiannya dua tahun kemudian. Dia menandatangani kontrak dengan 10x Entertainment dalam usahanya untuk memulai karir solo. ",
+            "https://upload.wikimedia.org/wikipedia/commons/c/c2/190419_Stray_Kids_%E5%AE%87%E7%8F%8D.png",
         ),
+
+
     )
     val listData: ArrayList<Member>
         get() {
